8b66f4628ce7de18318f4340895dbe87bfc199b5
MrRevillod <lrevillod2022@alu.uct.cl>
2022-09-12 13:57:18 -0300

    13:57 commit

--- classes.py
+++ classes.py
@@ -9,35 +9,35 @@ class Barco:
 class Contenedor_NP:
     capacidad: int = 12000
     tipo = "normal"
-    carga = ["solida"]
+    carga = ["solida","inerte"]
     productos  = []
 
 @dataclass
 class Contenedor_NG:
     capacidad: int = 24000
     tipo = "normal"
-    carga = ["solida"]
+    carga = ["solida", "inerte"]
     productos  = []
 
 @dataclass
 class Contenedor_RG:
     capacidad: int = 20000
     tipo = "refrigerada"
-    carga = ["solida", "normal", "liquido"]
+    carga = ["solida", "normal", "liquido", "inerte"]
     productos = []
 
 @dataclass
 class Contenedor_RP:
     capacidad: int = 10000
     tipo = "refrigerada"
-    carga = ["solida", "normal", "liquido"]
+    carga = ["solida", "normal", "liquido", "inerte"]
     productos = []
 
 @dataclass
 class Estanque:
     capacidad: int = 100000
     tipo = "normal"
-    carga = ["Gas", "liquido"]
+    carga = ["gas", "liquido"]
     productos  = []
 
 @dataclass
