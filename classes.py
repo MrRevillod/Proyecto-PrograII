9d5ebe9d492ffbeb69eaf2c145b43f8f4089b221
NicolasVZ86 <nikoval186995@gmail.com>
2022-09-12 10:42:27 -0300

    copia luciano

---
+++ classes.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+
+@dataclass
+class Barco:
+    costo: int = 1000000000
+    capacidad: int = 24000
+
+@dataclass
+class Contenedor_NP:
+    capacidad: int = 12000
+    tipo = "normal"
+    carga = ["solida"]
+    productos  = []
+
+@dataclass
+class Contenedor_NG:
+    capacidad: int = 24000
+    tipo = "normal"
+    carga = ["solida"]
+    productos  = []
+
+@dataclass
+class Contenedor_RG:
+    capacidad: int = 20000
+    tipo = "refrigerada"
+    carga = ["solida", "normal", "liquido"]
+    productos = []
+
+@dataclass
+class Contenedor_RP:
+    capacidad: int = 10000
+    tipo = "refrigerada"
+    carga = ["solida", "normal", "liquido"]
+    productos = []
+
+@dataclass
+class Estanque:
+    capacidad: int = 100000
+    tipo = "normal"
+    carga = ["Gas", "liquido"]
+    productos  = []
+
+@dataclass
+class Estanque_I:
+    capacidad: int = 100000
+    tipo = "inflamable"
+    carga = ["gas", "liquido"]
