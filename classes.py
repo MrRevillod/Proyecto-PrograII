9f2a3ef08cbbfd6061a95002bfccb020b442a0e8
NicolasVZ86 <nikoval186995@gmail.com>
2022-10-08 12:23:49 -0300

    avance clases contenedores

--- classes.py
+++ classes.py
@@ -1,14 +1,64 @@
 from dataclasses import dataclass
 
-@dataclass
-class Containt():
-    tipo_carga: str
-    masa: str
-    tamaño: int
 
 @dataclass
-class cP(Containt):
-    nombre = "Pequeño"
+class Producto:
+	tipo_Cont = str
+	tipo_Carga = str
+	masa = str
+	peso = int
+	cont_G = 0
+	cont_P = 0
+
+	def atributos(self, tipo_Carga, masax, pesox):
+		self.tipo_Carga = tipo_Carga
+		self.masa = masax
+		self.peso = pesox
+		if masax == "solida":
+			self.tipo_Cont = "contenedor"
+		else:
+			self.tipo_Cont = "estanque"
+		peso_Max_Por_Carga_Cont = [
+													["normal",24000],
+													["refrigerado", 20000],
+													["inflamable",22000]]
+		for x in range(len(peso_Max_Por_Carga_Cont)):
+			if tipo_Carga == peso_Max_Por_Carga_Cont[x][0]:
+				peso_Max = peso_Max_Por_Carga_Cont[x][1]
+				self.cant_Cont(pesox, peso_Max)
+				return
+
+	def cant_Cont(self, pesox, pesomax):
+		if pesox >= pesomax:
+			self.cont_G += pesox//pesomax
+			resto = pesox % pesomax
+			self.cant_Cont(resto, pesomax)
+		elif pesox > (pesomax/2) :
+			self.cont_P += 1
+			resto = (pesox % pesomax/2)
+			self.cant_Cont(resto, pesomax)
+		else:
+			self.cont_P += 1
+		return
+
 @dataclass
-class cG(Containt):
-    nombre = "Grande"
+class Vehiculos:
+	tren = int
+	avion = int
+	camion = int
+
+	def cantidad(self, cont_Totales):
+		if cont_Totales >= 250:
+			self.tren += cont_Totales // 250
+			resto = cont_Totales % 250
+			self.cantidad(resto)
+		elif cont_Totales >= 10:
+			self.avion += cont_Totales// 10
+			resto = cont_Totales % 10
+			self.cantidad(resto)
+		elif cont_Totales <= 1:
+			self.camion += 1
+			return
+		else:
+			self.avion += 1
+			return
--- proyectov3.py
+++ proyectov3.py
@@ -1,27 +1,16 @@
-import dataclasses
 import csv
 from classes import *
 
-cont_NS = [] ; cont_NL = [] ; cont_NG = []
-cont_RS = [] ; cont_RL = [] ; cont_RG = [] 
-cont_IS = [] ; cont_IL = [] ; cont_IG = [] 
+lista_Cont = [
+    ["normal", ["solida", ], ["liquida", ], ["gas", ]],
+    ["refrigerado", ["solida", ], ["liquida", ], ["gas", ]],
+    ["inflamable", ["solida", ], ["liquida", ], ["gas", ]]]
 
-pesos_normales      =   [cont_NS, cont_NL, cont_NG]
-pesos_refrigerados  =   [cont_RS, cont_RL, cont_RG]
-pesos_inflamables   =   [cont_IS, cont_IL, cont_IG]
-
-lista_tipo_cont  = [[pesos_normales,"normal"], [pesos_refrigerados,"refrigerado"], [pesos_inflamables,"inflamable"]]
-lista_tipo_cont2 = [pesos_normales, pesos_refrigerados, pesos_inflamables]
-
-sumas_Totales = []
-
-strings_for_prints = ["solida", "liquida", "gaseosa"]
-
-cant_cont = []
-
+lista_Vh = [[],[],[]]
 # Leer el archivo csv (retorna una matriz con cada producto como una lista
-#                        con sus respectivos atributos)
-def read_csv (x):
+#                        con sus respectivos atributos). 
+# 										Todos los valores	retornados son strings
+def read_csv(x):
     with open(x, 'r') as c:
         line = csv.reader(c)
         lista = list(line)
@@ -29,42 +18,32 @@ def read_csv (x):
 
 # Asigna la cantidad de contenedores a cada tipo de contenedor
 # (pesos_normales, pesos_refrigerados, pesos_inflamables)
-def main(lista, lista_pesos, tipo_prod):
-    for i in range(len(lista)):
-        if lista[i][2] == tipo_prod:
-            if lista[i][3] == "solida":
-                lista_pesos[0].append(int(lista[i][4]))
 
-            elif lista[i][3] == "liquida":
-                lista_pesos[1].append(int(lista[i][4]))
 
-            elif lista[i][3] == "gas":
-                lista_pesos[2].append(int(lista[i][4]))
+def lista_Contenedores(lista, lista_Cont):
+	cont_Totales = 0
+	for i in range(len(lista)):
+		for x in range(len(lista_Cont)):
+			if lista[i][2] == lista_Cont[x][0]:
+				for j in range(len(lista_Cont[0])):
+					if lista[i][3] == lista_Cont[x][j][0]:
+							objeto = Producto()
+							objeto.atributos(lista[i][2], lista[i][3],
+																	int(lista[i][4]))
+							print(objeto.cont_G + (objeto.cont_P)/2)
+							cont_Totales += ( objeto.cont_G + (objeto.cont_P)/2)
+							lista_Cont[x][j].append(objeto)
+							break
+			else:
+				continue
+	print(cont_Totales)
+	return cont_Totales
 
-# Muestra la cantidad de contenedores por tipo de contenedor, la masa de
-# de los productos y el tamaño del contenedor
-def Total_Contenedores(total_c, peso_max, tipo_C, masa):
-    cont_g = 0; cont_p = 0
-    if total_c >= peso_max:
-        cont_g += int(total_c / peso_max)
-        resto = total_c % peso_max 
-        if resto > (peso_max/2):
-            cont_p += 1
-            resto_menor = resto % (peso_max/2)
-        if resto <= (peso_max/2) or resto_menor <= (peso_max/2):
-            cont_p += 1
-    elif total_c > (peso_max/2):
-        cont_p += 1
-        resto_menor = total_c % (peso_max/2)
-        if resto <= (peso_max/2) or resto_menor <= (peso_max/2):
-            cont_p += 1
+def lista_vehiculos(cont_Totales):
+	
+	pass
 
-    if cont_p == 0:
-        cont_t = int(cont_g)
-    elif (cont_p%2) == 0:
-        cont_t = int(cont_g + (cont_p/2))
-    elif (cont_p%2) != 0:
-        cont_t = cont_g + (cont_p/2)
-    # print(f"Contenedores {tipo_C} grandes con masa {masa}: {cont_g}")
-    # print(f"Contenedores {tipo_C} pequeños con masa {masa}: {cont_p}")
-    return cont_t
+
+if __name__ == "__main__":
+	lista = read_csv("MOCK_DATA.csv")
+	lista_Contenedores(lista, lista_Cont)
