6d2bf981395ef65585b2da85999cc2b7f5ee9c5a
NicolasVZ86 <nikoval186995@gmail.com>
2022-11-18 21:58:32 -0300

    Entrega_2_envio_v1

--- kernel.py
+++ kernel.py
@@ -1,6 +1,5 @@
 import csv
 from classes import *
-from entrega_2 import *
 
 # lista ordenada para juntar y organizar los contenedores por tipo y masa
 lista_Cont = [
@@ -29,16 +28,34 @@ def lista_Contenedores(lista, lista_Cont):
 							["normal",24000],
 							["refrigerado", 20000],
 							["inflamable",22000]]
-	for i in range(len(lista)):
-		peso_Prod = int(lista[i][4])
-		for x in range(len(lista_Cont)):
-			if lista[i][2] == lista_Cont[x][0]:
-				for j in range(len(lista_Cont[0])):
-					if lista[i][3] == lista_Cont[x][j][0]:
-						for t in range(len(peso_Max_Por_Tipo_Cont)):
-							if lista[i][2] == peso_Max_Por_Tipo_Cont[t][0]:
+	for i in range(len(lista)): # Recorre los productos
+
+		peso_Prod = int(lista[i][4]) # Pone el peso en entero
+
+		for x in range(len(lista_Cont)): # Recorreo el tipo_dep de la lista_Cont
+
+			if lista[i][2] == lista_Cont[x][0]: # Si el tipo_dep del producto es =
+																					# tipo_dep de la lista_Cont
+
+				for j in range(len(lista_Cont[0])): # recorre las masas de la lista_Cont
+
+					if lista[i][3] == lista_Cont[x][j][0]:# Si las masas del producto y
+																								# la lista_Cont son =
+
+						for t in range(len(peso_Max_Por_Tipo_Cont)):# recorre otra lista
+																												# para tener los pesos
+																												# maximos
+
+							if lista[i][2] == peso_Max_Por_Tipo_Cont[t][0]: # verifica que el
+																															# el peso maximo
+																															# sea el correcto
 								pesomax = peso_Max_Por_Tipo_Cont[t][1]
-								arr = cant_Cont(peso_Prod, pesomax)
+								arr = cant_Cont(peso_Prod, pesomax) # saca la cantidad maxima de
+																										# contenedores con el peso
+																										# del producto y el maximo
+																										# segun el tipo_deposito
+								# arr es una lista que contiene la cantidad de contenedores
+								# grandes, pequeños ,y si tienen un resto 
 								for g in range(arr[0]):
 									obj = Deposito()
 									obj.atributos(int(lista[i][0]), lista[i][1],
@@ -66,6 +83,7 @@ def lista_Contenedores(lista, lista_Cont):
 # Esta función se utiliza para sacar la cantida de contenedores grandes
 # llenos, grandes con resto y/o pequeños con restos, esta implementada
 # en la función lista_Contenedores.
+
 def cant_Cont(pesox, pesomax):
 	cont_G_llenos = 0; cont_G = 0; cont_P = 0
 	if pesox >= pesomax:
@@ -91,6 +109,7 @@ def cant_Cont(pesox, pesomax):
 # cantidad de contenedores considerando que los pequeños ocupan la
 # mitad del espacio que los grandes, por lo que en vez de contar
 # como un entero, cada uno vale 0.5 respecto de uno grande
+
 def cont_Totales(lista_Cont):
 	cantidad_Total = float(0)
 	lista = []
@@ -107,6 +126,7 @@ def cont_Totales(lista_Cont):
 # retorna una lista con la cantidad de vehiculos, si hay un resto
 # se le asigna al vehiculo correspondiente, esto se utiliza en la
 # función Dep_en_Vh.
+
 def cant_Vh(cont_Totales):
 	tren = 0; avion = 0; camion = 0
 	lista = [[tren], [avion], [camion]]
@@ -143,10 +163,7 @@ def cant_Vh(cont_Totales):
 
 def Dep_en_Vh(cant_Vhs, cont_Totales, lista_Vh):
 	cant_Total_Vh = [250, 10, 1]
-	nom = ["trenes", "aviones", "camiones"]
 	for r in range(len(cant_Vhs)):
-		# print(f"Cantidad total de {nom[r]} : ", int(cant_Vhs[r][0])) # cantidad
-		# total por tipo de vehiculo
 		var = 0
 		for k in range(int(cant_Vhs[r][0])):
 			obj = Vehiculo()
